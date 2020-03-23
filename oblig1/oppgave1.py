d4a7b6789806477aa6827793e6cdfc3cfcd7c8cd
Håvard Hustoft <haavard.h.97@gmail.com>
2020-03-23 17:37:13 +0100

    ferdig

--- oblig1/oppgave1.py
+++ oblig1/oppgave1.py
@@ -4,33 +4,89 @@ import matplotlib.pyplot as plt
 import math
 
 def main():
-    image = imread('bilder/portrett.png', flatten=True)
+    original_image = imread('bilder/portrett.png', flatten=True)
     
-    """ plt.hist(image.ravel(), bins=256)
+    linear_transform(original_image)
+
+    image = imread('bilder/oppgave1/linear_transform_portrett.png',flatten=True)
+
+    maske = imread('bilder/geometrimaske.png')
+    
+    backward(image, maske)
+    forward(image,maske)
+
+    plt.figure()
+    plt.hist(image.ravel(), bins=256)
     plt.title('Histogram av portrett.png')
     plt.savefig('bilder/portrett_histogram.png')
+    
 
+
+def forward(image,maske):
+    m,n,k = maske.shape
+    m_,n_ = image.shape
+
+    new_image = np.ndarray((m,n))
+    for i in range(m_):
+        for j in range(n_):
+            x_,y_ = T(j,i)
+            y_rounded = int(y_)
+            x_rounded = int(x_)
+            if (x_rounded in range(0,n) and y_rounded in range(0,m)):
+                new_image[y_rounded][x_rounded] = image[i][j]
+    
+    plt.imsave('bilder/oppgave1/nytt_portrett_forlengs.png',new_image,cmap='gray')
+
+def backward(image,maske):
+    m,n,k = maske.shape
+    m_,n_ = image.shape
+    new_image_1 = np.ndarray((m,n))
+    new_image_2 = np.ndarray((m,n))
+
+    for i in range(m):
+        for j in range(n):
+            x_,y_ = T_inverse(j,i)
+            y_rounded = int(y_)
+            x_rounded = int(x_)
+            if (x_rounded in range(0,n_) and y_rounded in range(0,m_)):
+                new_image_1[i][j] = bilinear_interpolate(image, x_, y_) #bilineær
+                new_image_2[i][j] = image[y_rounded][x_rounded] # nærmeste nabo
+
+    plt.imsave('bilder/oppgave1/nytt_portrett_baklengs_bilinear.png',new_image_1,cmap='gray')
+    plt.imsave('bilder/oppgave1/nytt_portrett_baklengs_nabo.png',new_image_2,cmap='gray')
+
+def T(x,y):
+    x_ = 3.2*x - 3*y + 164.2
+    y_ = 2.338658147*x + 4.092651757*y - 299.600639
+    return x_,y_
+
+def T_inverse(x,y):
+    x_ = 0.20348837208868717251*x + 0.14916126572995922689*y + 11.276019829782152097
+    y_ = -0.11627906977206701599*x + 0.15910535011195650869*y + 66.761087818434295571
+    return x_,y_
+
+def bilinear_interpolate(in_image, x, y):
+    x0 = math.floor(x)
+    y0 = math.floor(y)
+    x1 = math.ceil(x)
+    y1 = math.ceil(y)
+
+    delta_x = x-x0
+    delta_y = y-y0
+
+    p = in_image[y0][x0] + (in_image[y1][x0] - in_image[y0][x0])*delta_y
+    q = in_image[y0][x1] + (in_image[y1][x1] - in_image[y0][x1])*delta_y
+
+    new_val = p + (q - p)*delta_x
+    return new_val
+
+
+def linear_transform(image):
     std = 64
     my = 127
 
-    new_image = linear_transform(image.copy(), std, my)
+    new_image = image.copy()
 
-    plt.figure()
-    plt.hist(new_image.ravel(),bins=256)
-    plt.title('Linear transform histogram,std=%.2f,mean=%.2f' % (np.std(new_image),np.mean(new_image)))
-    plt.savefig('bilder/linear_transform_histogram.png')
-
-    plt.figure()
-    plt.imshow(new_image, cmap='gray')
-    plt.title('Linear transform')
-    plt.savefig('bilder/linear_transform_portrett.png') """
-
-    geometric_transform(image)
-
-
-
-
-def linear_transform(image, std, my):
     n,m = image.shape
     old_std = np.std(image.ravel())
     old_my = np.mean(image.ravel())
@@ -40,40 +96,15 @@ def linear_transform(image, std, my):
 
     for i in range(n):
         for j in range(m):
-            image[i][j] = min(a*image[i][j] + b, 255)
-    return image
-
-
-
-def geometric_transform(image):
-    out_image = imread('bilder/geometrimaske.png', flatten=True)
-    out_image_2 = out_image.copy()
-    width, height = image.shape
-    new_width,new_height = out_image.shape
-
-    plt.figure()
-    plt.imshow(out_image, cmap='gray')
-    plt.show()
-
-    #forlengs
-
-    #baklengs
-    for x in range(new_width):
-        for y in range(new_height):
-            src_x = min(int(round(float(x)/float(new_width)*float(width))), width-1)
-            src_y = min(int(round(float(y)/float(new_height)*float(height))), height-1)
-            src = image[src_x][src_y]
-            out_image[x][y] = src
+            new_image[i][j] = min(a*new_image[i][j] + b, 255)
     
-    plt.figure()
-    plt.imshow(out_image,cmap='gray')
-    plt.savefig('bilder/nytt_portrett_baklengs.png')
+    plt.imsave('bilder/oppgave1/linear_transform_portrett.png',new_image, cmap='gray')
+    
+    plt.hist(new_image.ravel(),bins=256)
+    plt.title('Linear transform histogram,std=%.2f,mean=%.2f' % (np.std(new_image.ravel()),np.mean(new_image.ravel())))
+    plt.savefig('bilder/oppgave1/linear_transform_histogram.png')
 
-
-
-       
-                
-
+    
 
 
 if __name__ == "__main__":
--- oblig1/oppgave2.py
+++ oblig1/oppgave2.py
@@ -6,42 +6,47 @@ import sys
 from skimage import exposure
 from skimage import data, img_as_float
 
-def main(filename, bits_ut=8):
+def main(filename):
     image = imread(filename, flatten=True)
     m,n = image.shape
-
+    window_size = int(m/8)
     #global_utjevning(image, m, n)
-
-    s = 3 # sidelengden på kvadratet
-    new_height = m + s%(m+s)
-    new_width = n + s%(n+s)
-    out_image = np.ndarray((new_height,new_width))
+    m_ = m
+    n_ = n
+    if (m % window_size != 0):
+        m_ = m + int(window_size/2)
+    if (n%window_size != 0):
+        n_ = n + int(window_size)
+    
+    out_image = np.ndarray((m_,n_))
 
     plt.figure()
     plt.hist(image.ravel(), bins=256)
     plt.savefig('bilder/original_hist.png')
 
-    #skalerer bildet slik at vi ikke mister piksler ved bilderanden
-    for y in range(new_height):
-        for x in range(new_width):
-            src_y = min(int(round(float(y)/float(new_height)*float(m))), m-1)
-            src_x = min(int(round(float(x)/float(new_width)*float(n))), n-1)
+    #skalerer bildet
+    for y in range(m_):
+        for x in range(n_):
+            src_y = min(int(round(float(y)/float(m_)*float(m))), m-1)
+            src_x = min(int(round(float(x)/float(n_)*float(n))), n-1)
             src = image[src_y][src_x]
             out_image[y][x] = src
     plt.imsave('bilder/oppgave2/oppg2_skalert.png', out_image, cmap='gray')
-    #huang('bilder/oppgave2/oppg2_skalert.png')
 
-    #compare(filename)
-    #clahe(out_image, m, n, s)
-    huang('bilder/oppgave2/oppg2_skalert.png')
+    global_utjevning('bilder/oppgave2/oppg2_skalert.png')
+    ahe('bilder/oppgave2/oppg2_skalert.png',window_size)
+    huang('bilder/oppgave2/oppg2_skalert.png', window_size)
 
     
 
 
-def global_utjevning(image,m,n):
+def global_utjevning(filename, bits_ut=8):
+    image = imread(filename, flatten=True)
+    m,n = image.shape
     N = m*n
+    bins = 2**bits_ut
 
-    histogram = np.zeros(256)
+    histogram = np.zeros(bins)
     for i in range(len(image.ravel())):
         histogram[int(round(image.ravel()[i]))] += 1
     
@@ -59,171 +64,54 @@ def global_utjevning(image,m,n):
         for j in range(n):
             out_image[i][j] = T[int(image[i][j])]
     
-    plt.imsave('bilder/oppgave2/oppg2_utjevning.png', out_image, cmap='gray')
+    plt.imsave('bilder/oppgave2/oppg2_global.png', out_image, cmap='gray')
     fig = plt.figure()
     plt.hist(out_image.ravel(), bins=256)
     plt.title('Utjevnet histogram')
-    plt.savefig('bilder/oppgave2/oppg2_hist.png')
+    plt.savefig('bilder/oppgave2/oppg2_global_hist.png')
     plt.close(fig)
     
 
 
-def ahe(image, m, n, s):
-    N = m*n
-    G = 256
-
-
-    segment = np.ndarray((s,s))
-    # sliding window ahe
-    for i in range(s):
-        for j in range(s):
-            segment[i][j] = image[i+1][j+1]
-    c = find_cumulative_histogram(segment)
-    T = [(G-1)*c[k] for k in range(G)]
-    image[2][2] = T[int(image[2][2])]
-
-    for i in range(1,m-1):
-        for j in range(1, n-2):
-            for k in range(s):
-                segment[k][0] = segment[k][1]
-                segment[k][1] = segment[k][2]
-                segment[k][2] = image[i][j+2]
-            c = find_cumulative_histogram(segment)
-            T = [(G-1)*c[k] for k in range(G)]
-            image[i][j] = T[int(image[i][j])]
-
-
-    """ for i in range(1, new_height-1):
-        for j in range(1,new_width-1):
-            segment = np.ndarray((s,s))
-
-            for v in range(s):
-                segment[v] = out_image[i-(v-1)][j-1:j+s-1]
-            
-            c = find_cumulative_histogram(segment)
-            T = [(G-1)*c[k] for k in range(G)]
-            out_image[i][j] = T[int(out_image[i][j])] """
-    save(image)
-            
-
-def find_cumulative_histogram(segment):
-    m,n = segment.shape
-    N = m*n
-    normal_histogram = np.zeros(256)
-
-    for i in range(n):
-        for j in range(m):
-            normal_histogram[int(segment[i][j])] += 1
-    
-    normal_histogram = normal_histogram/N
-    normal_cumulative_histogram = np.zeros(256)
-    normal_cumulative_histogram[0] = normal_histogram[0]
-
-
-    for i in range(1,len(normal_histogram)):
-        normal_cumulative_histogram[i] = normal_cumulative_histogram[i-1] + normal_histogram[i]
-    
-    return normal_cumulative_histogram
-
-
-def clahe(image, m, n, s):
-    N = m*n
-    G = 256
-    cutoff = 0.5
-
-
-    segment = np.ndarray((s,s))
-    
-    # sliding window ahe
-    for i in range(s):
-        for j in range(s):
-            segment[i][j] = image[i][j]
-    c1 = find_cumulative_histogram_clahe(segment)
-    T1 = [(G-1)*c1[k] for k in range(G)]
-    image[1][1] = T1[int(image[1][1])]
-
-    for i in range(s,m-1):
-        for j in range(s, n-2):
-            for k in range(s):
-                segment[k][0] = segment[k][1]
-                segment[k][1] = segment[k][2]
-                segment[k][2] = image[i][j+2]
-            c = find_cumulative_histogram_clahe(segment)
-            T = [(G-1)*c[k] for k in range(G)]
-            image[i][j] = T[int(image[i][j])]
-    save(image, 'bilder/oppgave2/clahe_result_cutoff={}.png'.format(cutoff), 'bilder/oppgave2/clahe_hist_cutoff={}.png'.format(cutoff),cutoff)
-    
-
-def find_cumulative_histogram_clahe(segment):
-    m,n = segment.shape
-    N = m*n
-    histogram = np.zeros(256)
-
-    for i in range(n):
-        for j in range(m):
-            histogram[int(segment[i][j])] += 1
-    print(histogram)
-    normal_histogram = histogram/N
-    cutoff = np.max(normal_histogram)
-    
-    accumulator = 0
-    for i in range(256):
-        if (normal_histogram[i] > cutoff):
-            diff = normal_histogram[i] - cutoff
-            normal_histogram[i] -= diff
-            accumulator += diff
-    normal_histogram += accumulator/256
-    normal_cumulative_histogram = np.zeros(256)
-    normal_cumulative_histogram[0] = normal_histogram[0]
-
-
-    for i in range(1,len(normal_histogram)):
-        normal_cumulative_histogram[i] = normal_cumulative_histogram[i-1] + normal_histogram[i]
-    return normal_cumulative_histogram
-
-
-
-def huang(filename):
+def ahe(filename, window_size):
     image = imread(filename,flatten=True)
     m,n = image.shape
     N = n*m
-    w = 3 # window size
+    half = int(window_size/2)
     
     
-    for i in range(1,m-1):
+    for i in range(half,m-half):
         #Finner første histogrammet for hver rad
         hist = np.zeros(256)
-        for v in range(i-1, i+2):
-            for g in range(3):
+        for v in range(i-half, i+half+1):
+            for g in range(window_size):
                 hist[int(image[v][g])] += 1
-        cdh = find_cdh(hist, w)
-        pi = int(image[i][1])
+        cdh = find_cdh_ahe(hist, window_size)
+        pi = int(image[i][half])
         p = cdh[pi]*255
         
         #Denne løkken trekker fra siste kolonne og legger til den nye kolonnen til høyre
-        for k in range(-1,1):
+        for k in range(-half,half):
             hist[int(image[i+k][0])] -= 1
-            hist[int(image[i+k][3])] += 1
+            hist[int(image[i+k][window_size])] += 1
         
         if (p > 255):
-            image[i][1] = 255
+            image[i][half] = 255
         else:
-            image[i][1] = p
+            image[i][half] = p
         #legger til den nye pikselen i histogrammet
-        hist[int(image[i][1])] += 1
-        #fjerner den gamle verdien siden den ikke finnes i bildet lenger
+        hist[int(image[i][half])] += 1
+        #fjerner den gamle verdien da den ikke finnes i bildet lenger
         hist[pi] -= 1
         
-        for j in range(2,n-2):
-            cdh = find_cdh(hist, w)
+        for j in range(half+1,n-(half+1)):
+            cdh = find_cdh_ahe(hist, window_size)
             pi = int(image[i][j])
             p = cdh[pi]*255
-            
 
-            for k in range(-1,1):
-                hist[int(image[i+k][j-1])] -= 1
-                hist[int(image[i+k][j+2])] += 1
-            
+            for k in range(-half,half):
+                hist[int(image[i+k][j-half])] -= 1
+                hist[int(image[i+k][j+half+1])] += 1
             if (p > 255):
                 image[i][j] = 255
             else:
@@ -231,7 +119,7 @@ def huang(filename):
             
             hist[int(image[i][j])] += 1
             hist[pi] -= 1
-        cdh = find_cdh(hist,w)
+        cdh = find_cdh_ahe(hist,window_size)
         p = cdh[int(image[i][n-2])]
         old = [int(image[i][n-2])]
         if (p > 255):
@@ -239,20 +127,86 @@ def huang(filename):
         else:
             image[i][j] = p
 
-    save(image, 'bilder/oppgave2/huang.png', 'bilder/oppgave2/huang_hist.png')
-                
-    
+    save(image,'bilder/oppgave2/ahe.png','bilder/oppgave2/ahe_hist.png')
+            
 
-def find_cdh(histogram, w):
+def find_cdh_ahe(histogram, w):
     normal_histogram = histogram/(w*w)
     
-    lim = 0.05
+    cdh = np.zeros(256)
+    cdh[0] = normal_histogram[0]
+    for i in range(1,256):
+        cdh[i] = cdh[i-1] + normal_histogram[i]
+    return cdh
+
+
+
+def huang(filename, window_size):
+    image = imread(filename,flatten=True)
+    m,n = image.shape
+    N = n*m
+    half = int(window_size/2)
+    
+    
+    for i in range(half,m-half):
+        #Finner første histogrammet for hver rad
+        hist = np.zeros(256)
+        for v in range(i-half, i+half+1):
+            for g in range(window_size):
+                hist[int(image[v][g])] += 1
+        cdh = find_cdh_clahe(hist, window_size)
+        pi = int(image[i][half])
+        p = cdh[pi]*255
+        
+        #Denne løkken trekker fra siste kolonne og legger til den nye kolonnen til høyre
+        for k in range(-half,half):
+            hist[int(image[i+k][0])] -= 1
+            hist[int(image[i+k][window_size])] += 1
+        
+        if (p > 255):
+            image[i][half] = 255
+        else:
+            image[i][half] = p
+        #legger til den nye pikselen i histogrammet
+        hist[int(image[i][half])] += 1
+        #fjerner den gamle verdien da den ikke finnes i bildet lenger
+        hist[pi] -= 1
+        
+        for j in range(half+1,n-(half+1)):
+            cdh = find_cdh_clahe(hist, window_size)
+            pi = int(image[i][j])
+            p = cdh[pi]*255
+
+            for k in range(-half,half):
+                hist[int(image[i+k][j-half])] -= 1
+                hist[int(image[i+k][j+half+1])] += 1
+            if (p > 255):
+                image[i][j] = 255
+            else:
+                image[i][j] = p
+            
+            hist[int(image[i][j])] += 1
+            hist[pi] -= 1
+        cdh = find_cdh_clahe(hist,window_size)
+        p = cdh[int(image[i][n-2])]
+        old = [int(image[i][n-2])]
+        if (p > 255):
+            image[i][j] = 255
+        else:
+            image[i][j] = p
+
+    save(image, 'bilder/oppgave2/huang_window_size={}.png'.format(window_size), 'bilder/oppgave2/huang_hist_window_size={}.png'.format(window_size))
+    
+
+def find_cdh_clahe(histogram, w):
+    normal_histogram = histogram/(w*w)
+    lim = 0.025
     acc = 0
     for i in range(256):
         if (normal_histogram[i] > lim):
             diff = normal_histogram[i]-lim
-            normal_histogram[i] = normal_histogram[i]-diff
-            acc = acc + diff
+            normal_histogram[i] -= diff
+            acc += diff
     normal_histogram += acc/256
     
     cdh = np.zeros(256)
@@ -261,90 +215,13 @@ def find_cdh(histogram, w):
         cdh[i] = cdh[i-1] + normal_histogram[i]
     return cdh
 
-    
 
-    
-
-def compare(filename):
-    img = imread(filename)
-    p2, p98 = np.percentile(img, (2, 98))
-    img_rescale = exposure.rescale_intensity(img, in_range=(p2, p98))
-
-    # Equalization
-    img_eq = exposure.equalize_hist(img)
-
-    # Adaptive Equalization
-    img_adapteq = exposure.equalize_adapthist(img, clip_limit=0.03)
-
-    # Display results
-    fig = plt.figure(figsize=(8, 5))
-    axes = np.zeros((2, 4), dtype=np.object)
-    axes[0, 0] = fig.add_subplot(2, 4, 1)
-    for i in range(1, 4):
-        axes[0, i] = fig.add_subplot(2, 4, 1+i, sharex=axes[0,0], sharey=axes[0,0])
-    for i in range(0, 4):
-        axes[1, i] = fig.add_subplot(2, 4, 5+i)
-
-    ax_img, ax_hist, ax_cdf = plot_img_and_hist(img, axes[:, 0])
-    ax_img.set_title('Low contrast image')
-
-    y_min, y_max = ax_hist.get_ylim()
-    ax_hist.set_ylabel('Number of pixels')
-    ax_hist.set_yticks(np.linspace(0, y_max, 5))
-
-    ax_img, ax_hist, ax_cdf = plot_img_and_hist(img_rescale, axes[:, 1])
-    ax_img.set_title('Contrast stretching')
-
-    ax_img, ax_hist, ax_cdf = plot_img_and_hist(img_eq, axes[:, 2])
-    ax_img.set_title('Histogram equalization')
-
-    ax_img, ax_hist, ax_cdf = plot_img_and_hist(img_adapteq, axes[:, 3])
-    ax_img.set_title('Adaptive equalization')
-
-    ax_cdf.set_ylabel('Fraction of total intensity')
-    ax_cdf.set_yticks(np.linspace(0, 1, 5))
-
-    # prevent overlap of y-axis labels
-    fig.tight_layout()
-    plt.show()
-
-
-
-def plot_img_and_hist(image, axes, bins=256):
-    """Plot an image along with its histogram and cumulative histogram.
-
-    """
-    image = img_as_float(image)
-    ax_img, ax_hist = axes
-    ax_cdf = ax_hist.twinx()
-
-    # Display image
-    ax_img.imshow(image, cmap=plt.cm.gray)
-    ax_img.set_axis_off()
-
-    # Display histogram
-    ax_hist.hist(image.ravel(), bins=bins, histtype='step', color='black')
-    ax_hist.ticklabel_format(axis='y', style='scientific', scilimits=(0, 0))
-    ax_hist.set_xlabel('Pixel intensity')
-    ax_hist.set_xlim(0, 1)
-    ax_hist.set_yticks([])
-
-    # Display cumulative distribution
-    img_cdf, bins = exposure.cumulative_distribution(image, bins)
-    ax_cdf.plot(bins, img_cdf, 'r')
-    ax_cdf.set_yticks([])
-
-    return ax_img, ax_hist, ax_cdf
-
-def save(image, name, hist_name,cutoff=0):
+def save(image, name, hist_name):
     fig = plt.figure()
-    plt.hist(image.ravel(), bins=256)
-    plt.title('Histogram for clahe med cutoff={}'.format(cutoff))
+    plt.hist(image.ravel(), bins=256, density=True)
     plt.savefig(hist_name)
     plt.close(fig)
     plt.imsave(name, image, cmap='gray')
-
-
 
 
 if __name__ == "__main__":
